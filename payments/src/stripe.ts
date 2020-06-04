875523dd90c87af0e0454b565ee99a72e8cbf06c
Schult, John (js798p) <js798p@att.com>
2020-06-04 16:44:40 -0400

    fixed proxy issues for payment

--- payments/src/stripe.ts
+++ payments/src/stripe.ts
@@ -1,11 +1,5 @@
-import ProxyAgent from 'https-proxy-agent'
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_KEY!, {
   apiVersion: '2020-03-02',
-  // @ts-ignore
-  httpAgent: new ProxyAgent({
-    host: 'sub.proxy.att.com',
-    port: 8080,
-  }),
 })
